2fd56505b02d9f051103e22953a17823e78f69c5
zeeshan karamat <karamatzeeshan97@gmail.com>
2024-02-22 23:53:39 +0500

    my first code

---
+++ arithmatic-operator.cpp
@@ -0,0 +1,71 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a=7, b=9;
+     
+
+cout<<"Operators in C++" <<endl;
+
+cout<<"Thhese are the following operators in C++ " <<endl;
+cout<<endl;
+
+cout<<"Arithmatic Operators " <<endl;
+cout<<"Assignment Operators " <<endl;
+cout<<"Compariso Operators " <<endl;
+cout<<"Logical Operators " <<endl;
+cout<<endl;
+
+
+cout<<"Arithmatic operators in C++"<<endl;
+cout<<endl;
+cout<<"the value of a+b is " <<a+b<<endl;
+cout<<"the value of a-b is " <<a-b<<endl;
+cout<<"the value of a*b is " <<a*b<<endl;
+cout<<"the value of a/b is " <<a/b<<endl;
+cout<<"the value of a%b is " <<a%b<<endl;
+cout<<"the value of a++ is " <<a++<<endl;
+cout<<"the value of a-- is " <<a--<<endl;
+cout<<endl;
+
+
+
+cout<<"Assignment operators in C++"<<endl;
+cout<<endl;
+
+//Use to assigned values to variables.
+
+    int d=7, h=3;
+    char g='g';
+
+cout<<"The value of a is " <<a<<endl;
+cout<<"The value of b is " <<b<<endl;
+cout<<"The value of g is " <<g<<endl;
+cout<<endl;
+
+
+cout<<"Comparison operators in C++ "<<endl;
+cout<<endl;
+
+cout<<"The value of a==b " <<(a==b)<<endl;
+cout<<"The value of a!=b " <<(a!=b)<<endl;
+cout<<"The value of a<=b " <<(a<=b)<<endl;
+cout<<"The value of a>=b " <<(a>=b)<<endl;
+cout<<"The value of a< b " <<(a< b)<<endl;
+cout<<"The value of a> b " <<(a> b)<<endl;
+cout<<endl;
+
+
+cout<<"Logical Operators in C++"<<endl;
+cout<<endl;
+
+cout<<"The value of this logical and operator ((a==b) & (a<b)) " <<((a==b) & (a<b))<<endl;
+cout<<"The value of this logical or operator ((a==b) || (a<b))"  <<((a==b) || (a<b))<<endl;
+
+
+
+return 0;
+
+}
